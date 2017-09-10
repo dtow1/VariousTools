ad49d4a45031c3b63f3c40304ca8a7397baec029
Jason <jboy81@gmail.com>
2017-09-10 09:14:41 -0700

    Added the ability to read in processes from a conig file

--- src/com/jasonrboyer/systemtools/watchprocess/Configuration.java
+++ src/com/jasonrboyer/systemtools/watchprocess/Configuration.java
@@ -1,11 +1,15 @@
 package com.jasonrboyer.systemtools.watchprocess;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class Configuration {
 	private File configFile;
+	private BufferedReader fileReader;
 	private String fileName;
 	private ArrayList<String> processes = new ArrayList<String>();
 	
@@ -13,7 +17,6 @@ public class Configuration {
 		this("config.cfg");
 	}
 	public Configuration(String fileName){
-		System.out.println("checking for " + fileName);
 		this.fileName=fileName;
 		loadConfig();
 	} 
@@ -31,21 +34,52 @@ public class Configuration {
 		configFile = new File(fileName);
 
 		try {
+			configFile.createNewFile();
+			configFile.setReadable(true);
+			configFile.setWritable(true);
 			
-			System.out.println("create: " + configFile.createNewFile());
-			System.out.println("Set Readable: " + configFile.setReadable(true));
-			System.out.println("Set Writable: " + configFile.setWritable(true));
-			System.out.println(configFile.getName());
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println(configFile.exists());
-		System.out.println(getName());
+		
+		try {
+			fileReader = new BufferedReader(new FileReader(configFile));
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			String currentLine;
+			while((currentLine = fileReader.readLine()) != null) {
+				parseString(currentLine);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	
+	public void parseString(String toParse) {
+		int  colLoc = toParse.indexOf(':');
+		switch(toParse.substring(1,colLoc)) {
+			case ("process"):
+				String[] process = toParse.substring(colLoc+1,toParse.length()-1).split(",");
+				for(String prcs: process) {
+					processes.add(prcs);
+				}
+				break;	
+		}
+		
 	}
 	
 	public String getName() {
 		return configFile.getName();
+	}
+	
+	public ArrayList<String> getProcesses() {
+		return (ArrayList<String>) processes.clone();
 	}
 	
 	
--- src/com/jasonrboyer/systemtools/watchprocess/WatchProcess.java
+++ src/com/jasonrboyer/systemtools/watchprocess/WatchProcess.java
@@ -1,14 +1,22 @@
 package com.jasonrboyer.systemtools.watchprocess;
 
 import java.io.File;
+import java.util.ArrayList;
 
 public class WatchProcess {
 
 	public static void main(String[] args) {
-		
+		ArrayList<String> processes;
 		Configuration myConfig = new Configuration();
 		System.out.println("In main: " + myConfig.getName());
 		File testing = myConfig.getConfigFile();
+		
+		processes = myConfig.getProcesses();
+		
+		for(String prcs: processes) {
+			System.out.println("Main prcss");
+			System.out.println(prcs);
+		}
 		
 		
 
